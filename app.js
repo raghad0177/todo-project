4c8b127c5d472aa75d643d57f55a4ffefa2bd9eb
raghad0177 <abdullahraghad616@gmail.com>
2024-03-22 00:13:03 +0000

    lab6

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
-
-var name = prompt("Please enter your name:");
+var answers = [];
+function Questions() {
+    let questions = ["Are you Student? (yes/no)", "Do you have a jop? (yes/no)", "Do you like working as developer? (yes/no)"]
+    let question;
+    for (let x=0; x < questions.length; x++) {
+        question = prompt(questions[x]).trim().toLowerCase();
+        while (question !== "yes" && question !== "no") {
+            if (!question) {
+                alert("Invalid input.enter Yes or No.");
+            } else {
+                alert("Invalid input.enter Yes or No.");
+            }
+            question = prompt(questions[x]).trim().toLowerCase();
+        }
+        answers.push(question);
+    }
+    return answers;
+}
+function answerforqustions() {
+    Questions();
+    console.log("array of answers :" + answers)
+}
+const userAnswers = answerforqustions();
+var name1 = prompt("Please enter your name:");
 var gender = prompt("Please enter your gender (male or female):");
 var age = parseInt(prompt("Please enter your age:"));
-
-
-
-    if (age <= 0) {
-        alert("Invalid age!");
-    } 
-else{
-    if (gender === 'male') {      
-        var skipWelcome = confirm("Do you want to skip the welcoming message?");    
-        if (!skipWelcome)
-        {
-            alert("Welcome, Mr. " + name + "!");
-
-        }
-
-    } 
-    else if (gender === 'female') {
+if (age <= 0) {
+    alert("Invalid age!");
+}
+else {
+    if (gender === 'male') {
         var skipWelcome = confirm("Do you want to skip the welcoming message?");
-        if (!skipWelcome)
-        {
-            alert("Welcome, Ms. " + name + "!");
+        if (!skipWelcome) {
+            alert("Welcome, Mr. " + name1 + "!");
         }
     }
-
+    else if (gender === 'female') {
+        var skipWelcome = confirm("Do you want to skip the welcoming message?");
+        if (!skipWelcome) {
+            alert("Welcome, Ms. " + name1 + "!");
+        }
+    }
     else {
         var skipWelcome = confirm("Do you want to skip the welcoming message?");
-        if (!skipWelcome)
-        {
-            alert("Welcome, " + name + "!");
+        if (!skipWelcome) {
+            alert("Welcome, " + name1 + "!");
         }
     }
 }
- 
 
